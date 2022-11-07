a464490b04ea554437277baffb080314db7f3fcb
Jason Pak <55042077+JY2452@users.noreply.github.com>
2022-11-07 00:45:51 -0500

    add ui

---
+++ Assets/Scripts/UI/MenuUIHandler.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject inventoryMenu;
+    public GameObject tooltipMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        showMainMenu();
+    }
+
+    public void showMainMenu()
+    {
+        mainMenu.SetActive(true);
+        inventoryMenu.SetActive(false);
+        tooltipMenu.SetActive(false);
+    }
+
+    public void showInventoryMenu()
+    {
+        mainMenu.SetActive(false);
+        inventoryMenu.SetActive(true);
+        tooltipMenu.SetActive(false);
+    }
+
+    public void showTooltipMenu()
+    {
+        mainMenu.SetActive(false);
+        inventoryMenu.SetActive(false);
+        tooltipMenu.SetActive(true);
+    }
+}
