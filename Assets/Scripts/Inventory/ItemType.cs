fc9a010ba27f22b774e975090264b1bed52ae464
Andrew W. Chen <andrewac2015@gmail.com>
2022-11-11 17:52:32 -0500

    big import

--- Assets/Scripts/Inventory/ItemType.cs
+++ Assets/Scripts/Inventory/ItemType.cs
@@ -18,4 +18,5 @@ public enum ItemType
     CarrotSeed,
     Pumpkin,
     PumpkinSeed,
+    CobblePath,
 }
--- Assets/Scripts/Structure/StructureType.cs
+++ Assets/Scripts/Structure/StructureType.cs
@@ -7,4 +7,5 @@ public enum StructureType
     SheepStable,
     Plot,
     ChickenCoop,
+    CobblePath,
 }
